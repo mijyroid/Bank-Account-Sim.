da6366b58351b879f6b42296ca475a2021b021a9
Joyce J. <148932766+mijyroid@users.noreply.github.com>
2024-12-19 12:26:54 -0600

    Update main.cpp
    Removal of unnecessary IDE information

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 /*Program Statement
 A Program to carry out functions of a bank account
 */
